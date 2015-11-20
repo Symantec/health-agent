f581793103d12e7e8a07de0917cec6c9f39a0442
Richard Gooch <rg+github0@safe-mbox.com>
2015-11-19 18:06:08 -0800

    Add simple status bar to filesystems display.

--- probers/filesystems/html.go
+++ probers/filesystems/html.go
@@ -26,6 +26,7 @@ func (p *prober) writeHtml(writer io.Writer) {
 	fmt.Fprintln(writer, "    <th>Mount Point</th>")
 	fmt.Fprintln(writer, "    <th>Size</th>")
 	fmt.Fprintln(writer, "    <th>Used</th>")
+	fmt.Fprintln(writer, "    <th></th>")
 	fmt.Fprintln(writer, "  </tr>")
 	for _, fs := range fsList {
 		fs.writeHtml(writer)
@@ -53,5 +54,21 @@ func (fs *fileSystem) writeHtml(writer io.Writer) {
 	fmt.Fprintf(writer, "    <td><center>%s</td>\n", fs.mountPoint)
 	fmt.Fprintf(writer, "    <td><center>%s</td>\n", format.FormatBytes(fs.size))
 	fmt.Fprintf(writer, "    <td><center>%.1f%%</td>\n", usedPercent)
-	fmt.Fprintf(writer, "  </tr>\n")
+	fmt.Fprint(writer, "    <td>")
+	fs.writeHtmlBar(writer)
+	fmt.Fprintln(writer, "</td>")
+	fmt.Fprintln(writer, "  </tr>")
+}
+
+func (fs *fileSystem) writeHtmlBar(writer io.Writer) {
+	//usedBytes := fs.size - fs.free
+	bgColour := "green"
+	if fs.available < 1 {
+		bgColour = "red"
+	}
+	fmt.Fprint(writer, `<table border="0" style="width:200px">`)
+	fmt.Fprintf(writer,
+		"<tr><td style=\"width:100%%;background-color:%s\">&nbsp;</td></tr>",
+		bgColour)
+	fmt.Fprint(writer, "</table>")
 }
