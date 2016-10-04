164805ed31b46274337f0549b05ba3cd7fde93e1
Richard Gooch <rg+github0@safe-mbox.com>
2016-10-03 21:58:49 -0700

    Do not fail if /etc/health-agent/tests.d is missing.

--- cmd/health-agent/healthcheck.go
+++ cmd/health-agent/healthcheck.go
@@ -13,21 +13,25 @@ import (
 
 type testConfig struct {
 	Testtype  string `yaml:"type"`
-	Probefreq uint8 `yaml:omitempty`
+	Probefreq uint8  `yaml:omitempty`
 	Specs     testSpecs
 }
 
 type testSpecs struct {
 	Pidfilepath string
-	Urlpath string
-	Urlport uint
+	Urlpath     string
+	Urlport     uint
 }
 
-func setupHealthchecks(configDir string, pl *libprober.ProberList, logger *log.Logger) error {
+func setupHealthchecks(configDir string, pl *libprober.ProberList,
+	logger *log.Logger) error {
 	topDir := "/health-checks"
 	configdir, err := os.Open(path.Join(configDir, "tests.d"))
 	defer configdir.Close()
 	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
 		return err
 	}
 	configfiles, err := configdir.Readdir(0)
@@ -38,14 +42,15 @@ func setupHealthchecks(configDir string, pl *libprober.ProberList, logger *log.L
 		if configfile.IsDir() {
 			continue
 		}
-		data, err := ioutil.ReadFile(path.Join(configDir,configfile.Name()))
+		data, err := ioutil.ReadFile(path.Join(configDir, configfile.Name()))
 		if err != nil {
 			logger.Printf("Unable to read file %s\n%q", configfile.Name(), err)
 			return err
 		}
 		c := testConfig{}
 		if err := yaml.Unmarshal([]byte(data), &c); err != nil {
-			logger.Printf("Error unmarshalling file %s: %q", configfile.Name(), err)
+			logger.Printf("Error unmarshalling file %s: %q",
+				configfile.Name(), err)
 			return err
 		}
 		testname := strings.Split(configfile.Name(), ".")[0]
@@ -56,12 +61,13 @@ func setupHealthchecks(configDir string, pl *libprober.ProberList, logger *log.L
 	return nil
 }
 
-func makeProber(testname string, c *testConfig, logger *log.Logger) (libprober.RegisterProber) {
+func makeProber(testname string, c *testConfig,
+	logger *log.Logger) libprober.RegisterProber {
 	switch c.Testtype {
 	case "pid":
 		pidpath := c.Specs.Pidfilepath
 		if pidpath == "" {
-			return nil 
+			return nil
 		}
 		return pidprober.Makepidprober(testname, pidpath)
 	default:
