556a00f8ccb9de37ea341656eb5bf9ab1fbadbeb
mangai90 <itzmangai@gmail.com>
2016-11-14 17:30:55 -0800

    Fix transient true on healthy stat

--- probers/dns/probe.go
+++ probers/dns/probe.go
@@ -9,8 +9,9 @@ func (p *dnsconfig) probe() error {
 	starttime := time.Now()
 	if _, err := net.LookupIP(p.hostname); err != nil {
 		p.healthy = false
+	} else {
+		p.healthy = true
 	}
 	p.latency = int64(time.Since(starttime) / time.Millisecond)
-	p.healthy = true
 	return nil
 }
