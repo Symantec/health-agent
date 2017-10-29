6f4371bc87db0c7cd4ec7b874abefc6a6bea09f3
Richard Gooch <rg+github0@safe-mbox.com>
2017-10-29 14:12:56 -0700

    Handle integer wrapping for network interface data and packet counts.

--- probers/netif/api.go
+++ probers/netif/api.go
@@ -18,27 +18,33 @@ type netInterface struct {
 	carrier             bool
 	multicastFrames     uint64
 	rxCompressedPackets uint64
-	rxData              uint64
+	rxData              wrappingInt
 	rxDropped           uint64
 	rxErrors            uint64
 	rxFrameErrors       uint64
 	rxOverruns          uint64
-	rxPackets           uint64
+	rxPackets           wrappingInt
 	speed               uint64
 	txCarrierLosses     uint64
 	txCollisionErrors   uint64
 	txCompressedPackets uint64
-	txData              uint64
+	txData              wrappingInt
 	txDropped           uint64
 	txErrors            uint64
 	txOverruns          uint64
-	txPackets           uint64
+	txPackets           wrappingInt
 	probed              bool
 	lastProbeTime       time.Time
 	lastRxData          uint64
 	rxDataRate          uint64
 	lastTxData          uint64
 	txDataRate          uint64
+}
+
+type wrappingInt struct {
+	value  uint64
+	offset uint64
+	tmp    uint64
 }
 
 func Register(dir *tricorder.DirectorySpec) libprober.Prober {
